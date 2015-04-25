fa1a2a5fc8f41b33fd1d676c791647df88b552d9
sdockray <sean.patrick.dockray@gmail.com>
2015-04-25 23:08:33 +1000

    uwsgi change

--- sma/uwsgi.py
+++ sma/uwsgi.py
@@ -1,6 +1,16 @@
 import cherrypy
 from sma.server import ArchiveServer
 
+from sma.config import PATH_IMAGES, SERVER_PORT, SERVER_CSS, SERVER_IMAGES
+
 def application(environ, start_response):
-  app = cherrypy.tree.mount(ArchiveServer(), '/')
+	conf = {
+		'/'+PATH_IMAGES: {'tools.staticdir.on': True, 'tools.staticdir.dir': SERVER_IMAGES},
+		'/css': {'tools.staticdir.on': True, 'tools.staticdir.dir': SERVER_CSS},		
+	}
+	cherrypy.config.update({
+		'tools.sessions.on': True,
+		'server.socket_port': SERVER_PORT
+	})
+  app = cherrypy.tree.mount(ArchiveServer(), '/', config=conf)
   return cherrypy.tree(environ, start_response)

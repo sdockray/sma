26693db73ba3f1d1b97061933f0ae4ea7f106bde
sdockray <sean.patrick.dockray@gmail.com>
2015-04-29 23:22:35 +1000

    tryna fix thumbnail generating

--- sma/files.py
+++ sma/files.py
@@ -139,7 +139,7 @@ def thumbnail(path, dims=(150,150)):
 			img = Image.open(path)
 			if img.size[0]<=dims[0]:
 				wpercent = (dims[0] / float(img.size[0]))
- 				newh = int((float(img.size[1]) * float(wpercent)))
+ 				newh = int((float(img.size[1])*float(wpercent)))
 				img2 = img.resize((dims[0],newh), image.ANTIALIAS)
 				img2.save(tn(path))
 			else:

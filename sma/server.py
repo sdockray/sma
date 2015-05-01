f86053b113ec88846f1e71c995267c65ec46cb9d
sdockray <sean.patrick.dockray@gmail.com>
2015-05-01 10:51:28 +1000

    decode markdown for display

--- sma/server.py
+++ sma/server.py
@@ -87,7 +87,7 @@ def html(html, title="Social Media Archive", inject=""):
 			%s
 		</body>
 	</html>
-	""" % (title, inject, html)
+	""" % (title, inject, html.decode('utf-8', 'ignore'))
 
 # 
 class ArchiveServer(object):
